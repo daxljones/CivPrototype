44572b2558538d2dc6f9588620be1738217ef765
Dax Jones <48337068+daxljones@users.noreply.github.com>
2021-05-18 15:09:22 -0500

    In process of buidling menu

---
+++ Civilization Game/Assets/Scripts/Buttons/TownHall/BuildingButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingButton : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        transform.parent.GetComponent<MenuDisplay>().hideChildren();
+
+    }
+}
